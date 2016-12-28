125fed3bf40f09cfea6c84d9749a8da07f4ad015
Corey Norris <corey.norris@bluevolt.com>
2016-12-28 14:10:57 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name boilerplateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the boilerplateApp
+ */
+angular.module('boilerplateApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
